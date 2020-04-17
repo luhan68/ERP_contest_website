97aa6f82be22d53ccde02a7d5c39e1f8979f39b4
韩露 <hanlu@hanludeMacBook-Pro.local>
2020-04-17 16:16:09 -0700

    comment the user part'

--- app.py
+++ app.py
@@ -109,34 +109,34 @@ def leaderboard():
     return render_template("leaderboard.html", data = solution)
 
 
-# models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    teamname = db.Column(db.String(40))
-    output = db.Column(db.String(240))
-    time = 0
-    score = 0
+# # models
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     teamname = db.Column(db.String(40))
+#     output = db.Column(db.String(240))
+#     time = 0
+#     score = 0
 
-    def __init__(self,teamname,output):
-        self.teamname = teamname
-        self.output = output
-        self.time = 0
+#     def __init__(self,teamname,output):
+#         self.teamname = teamname
+#         self.output = output
+#         self.time = 0
 
-    def set_score(self,score):
-        self.score = score
+#     def set_score(self,score):
+#         self.score = score
     
-    def __repr__(self):
-        return 'Teamname: {}\nOutput: {}\nScore: {}\nTime: {}\n'.format(self.teamname,self.output,self.score,self.time)
+#     def __repr__(self):
+#         return 'Teamname: {}\nOutput: {}\nScore: {}\nTime: {}\n'.format(self.teamname,self.output,self.score,self.time)
 
-    def get_teamname(self):
-        return str(self.teamname)
+#     def get_teamname(self):
+#         return str(self.teamname)
 
-# forms
-from flask_wtf import FlaskForm
-from wtforms import SelectField, TextAreaField,StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,Length
+# # forms
+# from flask_wtf import FlaskForm
+# from wtforms import SelectField, TextAreaField,StringField, PasswordField, BooleanField, SubmitField
+# from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,Length
 
-class UserForm(FlaskForm):
-    teamname = TextAreaField('Teamname: ', validators=[DataRequired(), Length(min=1, max=40)])
-    output = TextAreaField('Enter your output: ', validators=[DataRequired(), Length(min=1, max=240)])
-    submit = SubmitField('Submit')
+# class UserForm(FlaskForm):
+#     teamname = TextAreaField('Teamname: ', validators=[DataRequired(), Length(min=1, max=40)])
+#     output = TextAreaField('Enter your output: ', validators=[DataRequired(), Length(min=1, max=240)])
+#     submit = SubmitField('Submit')
